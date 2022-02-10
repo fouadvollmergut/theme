db7ee93f29b1a2d752ba715d5feee486d38c2603
moritzgvt <hello@moritzgut.de>
2022-02-10 16:12:37 +0000

    Release v1.0.2

---
+++ template_direct.php
@@ -0,0 +1,13 @@
+<?php get_header(); /* Template Name: Direktausgabe */ ?>
+
+	<?php if( have_posts() ): while( have_posts() ): the_post(); $postID = get_the_ID(); ?>
+
+		<div class="gdymc_module">
+			<div class="modulePadding">
+				<?php the_content(); ?>
+			</div>
+		</div>
+
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
