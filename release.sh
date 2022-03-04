88ee897f6ae4f63deb406b6331464df67105d964
moritzgvt <hello@moritzgut.de>
2022-03-04 10:37:55 +0000

    Fix release versioning automation

--- release.sh
+++ release.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [ $# -eq 0 ]; then
     echo "Please supply the version number as an argument."
 fi
 
-read -p "Releaseing v$1. Did you push all necessary changes to master? (y/n)" -n 1 -r
+read -p "Releaseing v$1. Did you push all necessary changes to master? (y/n) " remember
+
+if [ $remember != 'y' ]; then 
+    echo "\nCome back when you are ready! Aborting Release …"
+    exit 1
+fi
 
 # Set release version
 git checkout -b release/v$1 origin/master
 
-sed -i '' -E 's/"version": ".*"/"version": "$1"/' package.json
-sed -i '' -E 's/Version: .*/Version: $1/' style.css
+sed -i '' -E "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json
+sed -i '' -E "s/Version: .*/Version: $1/" style.css
 
 git add package.json style.css
 git commit -m "Release $1"
@@ -34,3 +39,5 @@ git push origin --tags
 # Exit and delete build repo
 cd -
 rm -rf dist
+
+git co master
