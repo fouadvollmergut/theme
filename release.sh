d7532a2ae3ae6b19278839b3194db667bc29eb64
moritzgvt <hello@moritzgut.de>
2022-03-04 10:15:03 +0000

    Update release versioning

--- release.sh
+++ release.sh
@@ -4,6 +4,18 @@ if [ $# -eq 0 ]; then
     echo "Please supply the version number as an argument."
 fi
 
+read -p "Releaseing v$1. Did you push all necessary changes to master? (y/n)" -n 1 -r
+
+# Set release version
+git checkout -b release/v$1 origin/master
+
+sed -i '' -E 's/"version": ".*"/"version": "$1"/' package.json
+sed -i '' -E 's/Version: .*/Version: $1/' style.css
+
+git add package.json style.css
+git commit -m "Release $1"
+git push origin release/v$1
+
 # Build project
 npm run build:prod
 
