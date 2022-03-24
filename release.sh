6aaa819439b6b9f1a47e532ab0c69a6cb95558cc
moritzgvt <hello@moritzgut.de>
2022-03-24 14:27:07 +0000

    Update the release pipeline

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]; then
     echo "Please supply the version number as an argument."
 fi
 
-read -p "Releaseing v$1. Did you push all necessary changes to master? (y/n) " remember
+read -p "Releaseing Version $1. Did you push all necessary changes to master? (y/n) " remember
 
 if [ $remember != 'y' ]; then 
     echo "\nCome back when you are ready! Aborting Release …"
@@ -19,25 +19,8 @@ sed -i '' -E "s/Version: .*/Version: $1/" style.css
 
 git add package.json style.css
 git commit -m "Release $1"
-git push origin release/v$1
-
-# Build project
-npm run build:prod
-
-cd dist
-
-# Create repo for current release
-git init
-git remote add origin "git@github.com:fouadvollmer/theme.git"
-git checkout -b release/v$1
-
-git add .
-git commit -m "Release v$1"
-git tag -a v$1 -m "Release v$1"
+git tag -a v$1 -m "Release Version $1"
 git push origin --tags
-
-# Exit and delete build repo
-cd -
-rm -rf dist
+git push origin release/v$1
 
 git co master
