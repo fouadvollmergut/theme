e4ab621f4853761e101ed757e90c05c48bf0cb28
moritzgvt <hello@moritzgut.de>
2023-07-12 09:24:20 +0200

    Add wrapping conditions around constant function and class

--- includes/Constants/constants.php
+++ includes/Constants/constants.php
@@ -1,38 +1,42 @@
 <?php 
-  class CONSTANTS {
-    public $constants = null;
+  if ( !class_exists('CONSTANTS') ):
+    class CONSTANTS {
+      public $constants = null;
 
-    function GET( $path ) {
-      if( $this->constants === null ):
-        $locate = locate_template( 'includes/Constants/constants_object.php' );
+      function GET( $path ) {
+        if( $this->constants === null ):
+          $locate = locate_template( 'includes/Constants/constants_object.php' );
 
-        if( $locate ):
-          require $locate;
-          $this->constants = $constants;
-        else:
-          $this->constants = false;
+          if( $locate ):
+            require $locate;
+            $this->constants = $constants;
+          else:
+            $this->constants = false;
+          endif;
         endif;
-      endif;
-  
-      if( !$this->constants ) return false;
-  
-      $exploded = explode( '/', $path );
-  
-      $temp = $this->constants;
-  
-      foreach( $exploded as $key ):
-        if( !isset( $temp[ $key ] ) ) return null;
-  
-        $temp = $temp[ $key ];
-      endforeach;
-  
-      return $temp;
+    
+        if( !$this->constants ) return false;
+    
+        $exploded = explode( '/', $path );
+    
+        $temp = $this->constants;
+    
+        foreach( $exploded as $key ):
+          if( !isset( $temp[ $key ] ) ) return null;
+    
+          $temp = $temp[ $key ];
+        endforeach;
+    
+        return $temp;
+      }
     }
-  }
 
-  $constants_instance = new CONSTANTS;
+    $constants_instance = new CONSTANTS;
+  endif;
 
-  function CST () {
-    global $constants_instance;
-    return $constants_instance;
-  }
+  if ( !function_exists('CST') ):
+    function CST () {
+      global $constants_instance;
+      return $constants_instance;
+    }
+  endif;
